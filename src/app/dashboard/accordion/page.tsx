0cba0fbc4f7f0ebb72930f5b4f6e554192b867d4
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-12-20 16:49:56 -0600

    Install accordion and alert from shadcn

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,38 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+const items = [
+  {
+    id: 'item-1',
+    question: 'Is it accessible?',
+    answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    id: 'item-2',
+    question: 'How do I get started?',
+    answer:
+      'You can start by reading the documentation to learn more about the components and how to use them.',
+  },
+  {
+    id: 'item-3',
+    question: 'Can I use it on multiple projects?',
+    answer: 'Yes. It is licened under the MIT license. You can use it on multiple projects.',
+  },
+];
+
 export default function AccordionPage() {
   return (
-    <div>
-      <h1>Accordion Page</h1>
-    </div>
+    <Accordion type="single" collapsible className="w-full">
+      {items.map((item) => (
+        <AccordionItem key={item.id} value={item.id}>
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>{item.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
   );
 }
