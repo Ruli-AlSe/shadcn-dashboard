9fa5d4373f1a750e269b8b0cb3432f2f6953a3ee
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-12-21 00:07:42 -0600

    Install and test skeleton and slider components

---
+++ src/app/dashboard/skeleton/page.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
+
+const getData = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  return '123456789'.split('');
+};
+
+export default async function SkeletonPage() {
+  const data = await getData();
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      {data.map((item) => (
+        <Card key={item}>
+          <CardHeader className="flex flex-row">
+            <Image
+              src="https://github.com/shadcn.png"
+              alt="shadcn"
+              width={40}
+              height={40}
+              className="rounded-full mr-2 w-10 h-10"
+            />
+
+            <div>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Title</CardDescription>
+            </div>
+          </CardHeader>
+          <CardFooter className="flex justify-end">
+            <Button>See more</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
